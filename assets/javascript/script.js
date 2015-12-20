778669aa70355e9e7d8f9e11e16ec5de474621b5
Jamie Scovern <jamie.scovern@gmail.com>
2015-12-20 17:32:53 -0500

    committing all bug fixes, enhancements and new functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -12,23 +12,64 @@ var todos = [
   }
 ];
 
+
+if (getFromLocalStorage("todos-list") !== null)
+{
+  var todos = getFromLocalStorage("todos-list");  
+}
+
 // Increment global ID so they are unique.
 var id = todos.length;
 function incrementId() {
+  id = todos.length; //need to re-set it here, in case there are deletions
   id++;
   return id;
 }
 
 function renderTodos() {
-
   $('.todos').html('');
+
+  // if(todos.length === 0) {
+  //   return;
+  // }
 
   for (var i = 0; i < todos.length; i++) {
     var todo = todos[i];
     var checked = todo.complete ? 'checked' : ''
-    $('.todos').append("<li class='todo' data-id=" + todo.id + "><label><input class='toggle-todo' type='checkbox' " + checked + "/> " + todo.text + "</label></li>");
+    var completedClass="";
+    if(todo.complete) {
+      completedClass="completed";
+    }
+    $('.todos').append("<li class='todo' data-id=" + todo.id + "><label class='"+completedClass+"'><input class='toggle-todo' type='checkbox' " + checked + "/> " + todo.text + "</label><span class='deleteMe'> [x]</span></li>");
+    }
+
+    addToLocalStorage(todos);
+    
+    countTodos();
+    countStatus();
+
   }
-}
+
+  function countTodos() {
+    var count=todos.length;
+    $('.countTotal').text("Total: "+count);
+  }
+
+  function countStatus() {
+    var countComplete=0;
+    var countIncomplete=0;
+    for(var i=0; i <todos.length; i++) {
+      var todo=todos[i];
+      if (todo.complete) {
+        countComplete++;
+      } else {
+        countIncomplete++;
+      }
+    }
+    $('.countComplete').text("Complete: "+countComplete);
+    $('.countIncomplete').text("Incomplete: "+countIncomplete);
+
+  }
 
 function findById(id) {
   var todo;
@@ -42,6 +83,27 @@ function findById(id) {
   return todo;
 }
 
+function findArrayPositionByID(id) {
+  var position;
+
+  for(var i=0; i< todos.length; i++) {
+    if (todos[i].id === Number(id)) {
+      position = i;
+      return position;
+    }
+  }
+}
+
+function addToLocalStorage(myArray) {
+  // localStorage.setItem("todos-list", myArray);
+  localStorage["todos-list"] = JSON.stringify(todos); //local storage only allows strings and numbers - thise turns an array, into a string
+}
+
+function getFromLocalStorage(key) {
+  var myArray = JSON.parse(localStorage["todos-list"]); //this takes the string that is in local storage, and turns it back into the array
+  return myArray;
+}
+
 $(document).ready(function() {
   // Initialize with any existing todos.
   renderTodos();
@@ -50,27 +112,52 @@ $(document).ready(function() {
   $(document).on('change', '.toggle-todo', function(event) {
     var id = $(event.target).parent().parent().data('id');
     var todo = findById(id);
-
     todo.complete = event.target.checked;
 
     renderTodos();
   });
 
   // Bind to new todo form submission to create new todos.
-  $(document).on('submit', '.new-todo', function(event) {
+  $(document).on('click', '.add-todo', function(event) {
     event.preventDefault();
-
     var text = $('.todo-text').val();
+    if (text.length>0) {
 
-    var newTodo = {
-      id: incrementId(),
-      text: text,
-      completed: false
-    };
+      var newTodo = {
+        id: incrementId(),
+        text: text,
+        complete: false
+      };
 
     todos.push(newTodo);
 
     renderTodos();
+    
+    } else {
+      $('.alert').text("You entered a blank todo!");
+    }
+
+    $('.todo-text').val("");
+
   });
+
+  $(document).on('click', '.clear-completed', function(event) {
+    event.preventDefault();
+   for(var i=0; i< todos.length; i++) {
+      if (todos[i].complete === true) {
+        todos.splice(i,1);
+      }
+    }   
+    renderTodos();
+  });
+
+
+  $(document).on('click', '.deleteMe', function(event) {
+    var dataID = $(event.target).parent().data('id');
+    
+    todos.splice(findArrayPositionByID(dataID),1);
+    renderTodos();
+  });
+    
 
 });
